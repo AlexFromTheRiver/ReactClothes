a6b98252af265c80f9f1e45e1a74b56d0b00bc66
Alejandro Del Rio <delrio.alejandro@outlook.com>
2023-03-28 17:47:12 -0400

    Change to redux start

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
+// import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CategoriesProvider>
+      {/* <CategoriesProvider> */}
         <UserProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </UserProvider>
-      </CategoriesProvider>
+      {/* </CategoriesProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
